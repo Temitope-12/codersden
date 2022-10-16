f90d52d23b3b1677cbf7d0cdb90621cd4863f0bb
unknown <ibikunletemmytee@gmail.com>
2022-10-16 23:49:28 +0100

    level2

---
+++ level2.js
@@ -0,0 +1,7 @@
+console.log(\'There is no exericise better for the heartthan reaxching down and lifting people up.\' by john Holmes teaches us to help one another.)
+console.log(\"Love is not patronizing and charity isn't about pity, itis about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\")
+
+let num='10'
+let nums=10
+
+console.log(num === nums)
