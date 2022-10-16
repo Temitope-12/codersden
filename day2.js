5c9eeaf435048e978c38e69bd72a48ab175c5836
unknown <ibikunletemmytee@gmail.com>
2022-10-16 23:13:24 +0100

    day2

---
+++ day2.js
@@ -0,0 +1,8 @@
+let challenge= "30 Days of javaScript."
+console.log(challenge)
+
+console.log(challenge.length)
+
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(0))
